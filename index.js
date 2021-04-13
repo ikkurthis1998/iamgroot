79249addfc2f62f8399523008b50f325d861efb9
Sreemannarayana Ikkurthi <71547157+ikkurthis1998@users.noreply.github.com>
2021-04-13 17:28:10 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,30 @@
+var inputTextElement = document.querySelector("#input");
+var translateButtonElement = document.querySelector("#button");
+var outputTextElement = document.querySelector("#output");
+
+const translateApiURL = "https://api.funtranslations.com/translate/groot.json";
+
+function URL(text) {
+    return translateApiURL + "?" + "text=" + text;
+}
+
+function errorHandle(error) {
+    alert("sorry, server is not responding", error);
+}
+
+function clickEventHandler() {
+
+    function output(response) {
+        response = response.json();
+    }
+
+    var inputText = inputTextElement.value;
+    var finalURL = URL(inputText);
+    fetch(finalURL)
+        .then(response => response.json())
+        .then(json => {outputTextElement.innerText = json.contents.translated})
+        .catch(errorHandle());;
+}
+
+
+translateButtonElement.addEventListener("click", clickEventHandler);
